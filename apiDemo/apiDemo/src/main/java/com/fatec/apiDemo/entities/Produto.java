1d1829d3d51499d79fa79956a61983fc830228db
Tiago Santos <tiagoalbertoo17@gmail.com>
2025-04-02 21:42:13 -0300

    fix:Corrigindo entidades

---
+++ apiDemo/apiDemo/src/main/java/com/fatec/apiDemo/entities/Produto.java
@@ -0,0 +1,57 @@
+package com.fatec.apiDemo.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_produtos")
+@Getter
+@Setter
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome_prod", nullable = false)
+    private String nome_prod;
+
+    @Column(columnDefinition = "Text", nullable = false)
+    private String descricao;
+
+    @Column(name = "preco", nullable = false)
+    private double preco;
+
+    @Column(name = "disponivel")
+    private boolean disponivel;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria cat_prod;
+
+    public Produto() {}
+
+    public Produto(String nome_prod, String descricao, double preco, boolean disponivel, Categoria cat_prod) {
+        this.nome_prod = nome_prod;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.disponivel = disponivel;
+        this.cat_prod = cat_prod;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return id == produto.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
