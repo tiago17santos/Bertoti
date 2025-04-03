c31333cc9083f82e20d3ffb55c5baf20efa1f0d9
Tiago Santos <tiagoalbertoo17@gmail.com>
2025-04-02 22:31:04 -0300

    feat: Organizando arquivos

---
+++ apiDemo/apiDemo/src/main/java/com/fatec/apiDemo/entities/Categoria.java
@@ -0,0 +1,29 @@
+package com.fatec.apiDemo.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_categoria")
+@Getter
+@Setter
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="nome_cat",length=200,nullable=false)
+    private String nome_cat;
+
+    @OneToMany
+    private List<Produto> produtos;
+
+    public Categoria() {}
+
+    public Categoria( String nome) {
+        this.nome_cat = nome;
+    }
+}
