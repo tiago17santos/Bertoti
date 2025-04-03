6ac3cb57d1913cc621a241383e81dcf56f4252a9
Tiago Santos <67645178+tiago17santos@users.noreply.github.com>
2025-04-02 21:57:05 -0300

    Delete Lab lll/apiDemo directory

--- Lab lll/apiDemo/apiDemo/src/main/java/com/fatec/apiDemo/repositories/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fatec.apiDemo.repositories;
-
-import com.fatec.apiDemo.entities.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
